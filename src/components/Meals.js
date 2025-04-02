c06a9920b4be3e4ae66c52d5d93d83d70964b766
Annaliisa <annaliisa.tamme@voco.ee>
2025-04-02 11:55:54 +0300

    ylesanne 3

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,9 +1,27 @@
+import React, { useEffect, useState } from 'react'
+
 const Meals = () => {
+    const [meals, setMeals] = useState([])
+
+    useEffect(() => {
+        const fetchMeals = async () => {
+            try { 
+                const response = await fetch("http://localhost:3001/meals");
+                const data = await response.json();
+                setMeals(data);
+                console.log(data);
+            } catch (error) {
+                console.error("Error fetching meals:", error);
+            }
+        }
+        fetchMeals();
+    }, [])
     return (
         <ul id="meals">
-            { 
-                'Create list of meals, using fetch data from backend'
-            }
+                {meals.map((meal, index) => (
+                    console.log(meal)
+                ))}
+               
         </ul>
     )
 }
