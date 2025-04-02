dc102e82eccd0397982ba88112a5d26975c117a2
Annaliisa <annaliisa.tamme@voco.ee>
2025-04-02 12:41:15 +0300

    ylesanne 5

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -1,11 +1,14 @@
 const MealItem = ({ meal }) => {
+
+    const price = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'EUR' }).format(meal.price)
+
     return (
         <li className="meal-item">
             <article>
                 <img src={require(`../assets/${meal.image}`)} alt={meal.name}/>
                 <div>
                     <h3>{meal.name}</h3>
-                    <p className="meal-item-price">{meal.price}</p>
+                    <p className="meal-item-price">{price}</p>
                     <p className="meal-item-description">{meal.description}</p>
                 </div>
                 <p>
